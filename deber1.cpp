2a0322052c82ed0bb2eb244b68021db47e369a45
Ism4el06 <ismaarboleda2006@gmail.com>
2024-11-24 22:10:07 -0500

    Clovermejor humos

---
+++ deber1.cpp
@@ -0,0 +1,22 @@
+#include <iostream> // Incluye la biblioteca estándar de entrada/salida
+using namespace std;
+
+int main() {
+    double cantidadKw, precioKw, totalPagar;
+
+    cout << "Ingrese la cantidad de Kw consumidos: ";
+    cin >> cantidadKw;
+    cout << "Ingrese el precio por Kw: ";
+    cin >> precioKw;
+
+    if (cantidadKw > 700) {
+        double excesoKw = cantidadKw - 700;
+        totalPagar = (700 * precioKw) + (excesoKw * precioKw * 1.05);
+    } else {
+        totalPagar = cantidadKw * precioKw;
+    }
+
+    cout << "El total a pagar es: " << totalPagar << " unidades monetarias." << endl;
+
+    return 0;
+}
